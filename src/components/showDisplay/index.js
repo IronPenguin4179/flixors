27f759cc83534a691ee904fe0387286b8649f0db
“[Justin <jrwalker314@yahoo.com>
2019-04-30 18:12:27 -0700

    Clean up ShowDisplay and reduce from class to function.

--- src/components/showDisplay/index.js
+++ src/components/showDisplay/index.js
@@ -3,23 +3,23 @@ import ShowBlock from './showBlock';
 import ShowInfo from './showInfo';
 import './styles.scss';
 
-class ShowTable extends Component {
-  render(){
-    if(this.props.page === false)
-      return (
-        <span>
-          {this.props.names.map((item, index) => (
-            <div className="box" data-test='box' key={index}>
-              <ShowBlock id={item.show_id} name={item.name} img={item.poster_path} changePageToShow={this.props.changePageToShow}/> 
-            </div>
-          ))}
-        </span>
-      );
-    else
-      return (
-        <ShowInfo data={this.props.showData} />
-      )
-  };
+const ShowTable = (props) => {
+  if(props.page === false)
+
+    return (
+      <span>
+        {props.names.map((item, index) => (
+          <div className="box" data-test='box' key={index}>
+            <ShowBlock id={item.show_id} name={item.name} img={item.poster_path} changePageToShow={props.changePageToShow}/> 
+          </div>
+        ))}
+      </span>
+    );
+  else
+
+    return (
+      <ShowInfo data={props.showData} />
+    )
 };
 
 export default ShowTable;
