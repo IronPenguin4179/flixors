50a1f38a6c836f5a60385104d1913a950002bc19
“[Justin <jrwalker314@yahoo.com>
2019-04-30 18:03:54 -0700

    Clean up Header and reduce it from class to function.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchBar from './../searchBar';
 import Logo from './../../assets/graphics/logo.png';
 import './styles.scss';
 
-class Header extends Component {
-    render() {
-      return (
-        <header data-test="headerComponent">
-            <div className="wrap">
-              <div className="logo">
-                <button onClick={() => {this.props.changePageToTable()}}>
-                  <img data-test="logoIMG" src={Logo} alt="Logo" />
-                </button>
-              </div>
-              <div className="search">
-                <SearchBar allProps={this.props}/>
-              </div>
-            </div>
-        </header>
-      )
-    }
+const Header = (props) => {
+  return (
+    <header data-test="headerComponent">
+      <div className="wrap">
+        <div className="logo">
+          <button onClick={() => {props.changePageToTable()}}>
+            <img data-test="logoIMG" src={Logo} alt="Logo" />
+          </button>
+        </div>
+        <div className="search">
+          <SearchBar allProps={props}/>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
