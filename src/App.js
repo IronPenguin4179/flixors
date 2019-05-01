012697908f6bf45a35fc29bf39df5a4167a07707
“[Justin <jrwalker314@yahoo.com>
2019-04-30 18:01:04 -0700

    Clean up App and add comments.

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ class App extends Component {
       searchData: "No search data yet",
       names: ["Test1","Test2","Test3"]
     };
+    
     this.handleDataCallback = this.handleDataCallback.bind(this);
     this.searchTyping = this.searchTyping.bind(this);
   }
   
+  // Sets up initial call to database for popular shows.
   componentWillMount() {
-    axios.get('https://api.themoviedb.org/3/trending/tv/week?api_key=fb6a1d3f38c3d97f67df6d141f936f29')
-    .then((response) => {this.prepDataForTable(response.data.results)})
-    this.setNames(this.state.searchData)
+    this.resetSearchData()
   }
   
   render () {
@@ -40,31 +40,32 @@ class App extends Component {
       </div>
     );
   }
+
+  // Sets page to false and resets show data to popular items.
+  resetSearchData = () => {
+    axios.get('https://api.themoviedb.org/3/trending/tv/week?api_key=fb6a1d3f38c3d97f67df6d141f936f29')
+    .then((response) => {
+      const newNames = this.setNames(response.data.results);
+      this.setState({names: newNames, searchData: response.data.results})
+    })
+  }
   
-  showSelect(id) {
+  // Resets search data and page display 
+  changePageToTable = () => {
+    this.resetSearchData()
+    this.setState({page: false})
+  }
+  
+  // Searches for specified show and changes page state to true allowing ShowDisplay to render the shows ShowInfo
+  changePageToShow = (id) => {
     axios.get('https://api.themoviedb.org/3/tv/'+id+'?api_key=fb6a1d3f38c3d97f67df6d141f936f29')
     .then((response) => {
       this.setState({showData: response.data})
     })
-  }
-
-  prepDataForTable = (data) => {
-    const newNames = this.setNames(data);
-    this.setState({names: newNames, searchData: data})
-  }
-
-  prepDataForShow = (data) => {
-    this.setState({showData: data})
-  }
-
-  changePageToTable = () => {
-    this.setState({page: false, showData:""})
-  }
-
-  changePageToShow = (id) => {
-    this.showSelect(id);
     this.setState({page: true})
   }
+
+  // Reorganizes searchData for presetation by ShowDisplay
   setNames = (list) => {
     const newNames = [];
     for (let i=0;i<list.length;i++) {
@@ -77,10 +78,13 @@ class App extends Component {
     };
     return newNames;
   }
+
+  // Used for retreiving data from the SearchBar query.
   handleDataCallback = (data) => {
     this.setState({searchData: data, names: this.setNames(data)})
-    console.log(this.state.names)
   }
+
+  // Updates onChanges from searchBar
   searchTyping(msg) {
     this.setState({search: msg});
   }
