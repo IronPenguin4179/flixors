410696c7daf469f233e359a6cb8cd46cd8106a14
“[Justin <jrwalker314@yahoo.com>
2019-04-30 18:07:53 -0700

    Clean up SearchBar and reduce from class to function.

--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -1,43 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import './styles.scss';
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props)
-    this.handleQuery = this.handleQuery.bind(this);
-  }
-  
-  handleQuery = (event) => {
+const SearchBar = (props) => {
+
+  // Handles callback to App and queries database for searched info
+  const handleQuery = (event) => {
       event.preventDefault();
-      const searchString = 'https://api.themoviedb.org/3/search/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&query='+this.props.allProps.search;
+      const searchString = 'https://api.themoviedb.org/3/search/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&query='+props.allProps.search;
       axios.get(searchString).then(response => {
           const results = response.data.results;
           console.log("queryDb successful.")
-          this.props.allProps.getQueryResults(results);
+          props.allProps.getQueryResults(results);
       });
   };
 
-  handleChange = (event) => {
-    this.props.allProps.searchTyping(event.target.value)
+  // Handles change callback
+  const handleChange = (event) => {
+    props.allProps.searchTyping(event.target.value)
   }
-
-  render() {
-    return (
-      <section>
-        <form onSubmit={this.handleQuery}>
-          <input type="text" value={this.props.allProps.search} onChange={this.handleChange} />
-          <input type="submit" value="Submit" onClick={this.props.allProps.changePageToTable}/>
-        </form>
-      </section>
-    )
-  }
+  
+  return (
+    <section>
+      <form onSubmit={handleQuery}>
+        <input type="text" value={props.allProps.search} onChange={handleChange} />
+        <input type="submit" value="Submit" onClick={props.allProps.changePageToTable}/>
+      </form>
+    </section>
+  )
 }
 
 export default SearchBar;
-
-       // <form>
-       //   <input type="text" id="searchInput" name="searchQuery" placeholder="Search..." />
-       // </form>
-
-  
